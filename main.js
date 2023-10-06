86034c97511f80ef1d94691d56180895c61e8628
Daler <dsarifkulo021@gmail.com>
2023-10-06 19:39:14 +0500

    fixed

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+let inp = document.querySelector('input')
+let btn = document.querySelector('button')
+let h1 = document.querySelector('h1')
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector('#counter'))
+console.log(
+	JSON.parse(localStorage.getItem('title'))
+);
+
+inp.onkeyup = () => {
+	h1.innerHTML = inp.value
+}
+
+btn.onclick = () => {
+	let todo = {
+		id: 1,
+		task: inp.value,
+		isDone: false,
+		time: new Date().getHours() + ":" + new Date().getMinutes()
+	}
+
+
+	localStorage.setItem('title', JSON.stringify(todo))
+}
