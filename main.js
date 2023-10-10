04c331c541e8910974c2683182f3b3063cb52b65
RT <gulsanambegmatova@gmail.com>
2023-10-10 21:14:28 +0500

    home page,reg page,log in page

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
 
 let rightside = document.querySelector(".rightside")
-let header = document.querySelector("header")
+let header = document.querySelector(".header")
 let nameWelcome = document.querySelector('.name')
 let user = JSON.parse(localStorage.getItem("user"))
 let mail = document.querySelector(".mail")
+let cards = document.querySelector(".cards")
+let table = document.querySelector("table")
+for (let i = 0; i < 4; i++) {
+    cards.innerHTML += `        <div class="card">
+    <h3>Visa</h3>
+    <p>Rub</p>
+  </div>`
+}
+for (let i = 0; i < 6; i++) {
+    table.innerHTML += `      <tr>
+    <td>
+      1232312
+    </td>
+    <td>VISA</td>
+    <td>Автомобиль</td>
+    <td>414,000,000</td>
+    <td>4 дня назад</td>
+  </tr>`
+    
+}
 if (user!== null) {
     mail.innerHTML = user.email
     header.innerHTML = `      <div class="leftside">
