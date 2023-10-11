d7946cc7bb928c924bcef28af397a4445c25987d
RT <gulsanambegmatova@gmail.com>
2023-10-11 17:33:43 +0500

    register with fake back

--- pages/login/script.js
+++ pages/login/script.js
@@ -2,9 +2,27 @@ let form = document.querySelector('form')
 let inps = document.querySelectorAll('input')
 let a = document.querySelector(".button")
 console.log(a.href);
+
 a.onclick = () => {
-    if (JSON.parse(localStorage.getItem("user")).email === inps[0].value && JSON.parse(localStorage.getItem("user")).password === inps[1].value ) {
-        console.log("a");
-        a.href = "/index.html"
+    fetch(' http://localhost:7000/users', {
+            method: 'GET',
+        })
+        .then(res => res.json())
+        .then(res => reload(res))
+
+
+    // if (JSON.parse(localStorage.getItem("user")).email === inps[0].value && JSON.parse(localStorage.getItem("user")).password === inps[1].value) {
+    //     a.href = "/index.html"
+    // }
+}
+function reload(data) {
+    for (let i = 0; i < data.length; i++) {
+        const element = data[i];
+        console.log(element.email);
+        
+        if (inps[0].value ==element.email||inps[1].value ==element.password) {
+            a.href = "/index.html"
+            
+        }
     }
 }
--- pages/register/script.js
+++ pages/register/script.js
@@ -1,7 +1,7 @@
 let form = document.querySelector('form')
 let inps = document.querySelectorAll('input')
 let a = document.querySelector(".button")
-a.onclick = (event) =>{
+a.onclick = (event) => {
     let error = false
     inps.forEach(inp => {
         if (inp.value.length === 0) {
@@ -15,7 +15,7 @@ a.onclick = (event) =>{
     if (error) {
         alert('go home')
     } else {
-        a.href = "/pages/login/index.html"
+
         submit()
     }
 }
@@ -26,9 +26,33 @@ function submit() {
 
     fm.forEach((value, key) => {
         user[key] = value
-        user["isreg"] = false
     })
-    // fetch()
+    fetch("http://localhost:7000/users?email=" + user.email)
+        .then(res => res.json())
+        .then(res => {
+            if (res.length > 0) {
+                alert("This email was registered before")
+            } else {
+                post()
+            }
+        })
+    function post() {
+        fetch("http://localhost:7000/users", {
+            method: "post",
+            body: JSON.stringify(user),
+            headers: {
+                "Content-type": "application/json"
+            }
+        })
+            .then(res => {
+                if (res.status === 200 || res.status === 201) {
+                    localStorage.setItem("user", JSON.stringify(user))
+                    a.href = "/pages/login/"
+                }
+            })
+    }
+
+
     console.log(user);
-    localStorage.setItem("user", JSON.stringify(user))
+
 }
