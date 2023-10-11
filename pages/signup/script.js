07f918d982503924e12a93ae23b5a3a547ba525f
saloxitdinovv <134138858+saloxitdinovv@users.noreply.github.com>
2023-10-11 18:28:02 +0500

    ff

--- pages/signup/script.js
+++ pages/signup/script.js
@@ -32,7 +32,32 @@ function submit() {
         user[key] = value;
     });
 
-    console.log(user);
+    fetch('http://localhost:7000/users?email=' + user.email)
+        .then(res => res.json())
+        .then(res => {
+            if (res.length > 0) {
+                alert('Эта почта уже зарегистрирована')
+            } else {
+                post()
+            }
+        })
+
+    function post() {
+        fetch('http://localhost:7000/users', {
+                method: "post",
+                body: JSON.stringify(user),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+            .then(res => {
+                if (res.status === 200 || res.status === 201) {
+                    localStorage.setItem('user', JSON.stringify(user));
+                    location.assign('/pages/signin')
+                }
+            })
+    }
+
 
     localStorage.setItem('user', JSON.stringify(user));
     window.location.href = "/pages/signin/";
