d34d623dcc6ef0437bfcead3815e7fffb30a2e17
saloxitdinovv <134138858+saloxitdinovv@users.noreply.github.com>
2023-10-23 17:19:30 +0500

    gotovo

--- pages/cardID/index.js
+++ pages/cardID/index.js
@@ -3,6 +3,7 @@ import { getData } from "../../modules/http"
 import { transactions } from "../../modules/transactions";
 
 let userName = document.querySelector('h3')
+let userBalance = document.querySelector('.balance')
 let tbody = document.querySelector('tbody')
 let card = null
 
@@ -12,12 +13,16 @@ let id = location.search.split('=').at(-1)
 
 getData('/cards/' + id)
     .then(res => {
-        userName.innerHTML = res?.name
         card = res
+        userName.innerHTML = card.name
+        userBalance.innerHTML = `Balance: ${card.balance}`
         getData('/transactions?card_id=' + id)
             .then(res => transactions(res, tbody))
     })
 
 
+let flippingCard = document.querySelector('.card');
 
-// userName.innerHTML = user?.name
+flippingCard.ondblclick = () => {
+    flippingCard.classList.toggle('flipped');
+}
